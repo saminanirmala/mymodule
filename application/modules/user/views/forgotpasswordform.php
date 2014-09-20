9f3a04b84472fc552227486f9e930c4921f96557
saminanirmala <samina.tuladhar7@gmail.com>
2014-09-20 08:16:17 +0545

    view added

---
+++ application/modules/user/views/forgotpasswordform.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    <h1>Forgot your password</h1>
+    </head>
+    <body>
+        <form name="" method="POST">
+            Email:<input type="text" name="identity"><br/>
+            <input type="submit" name="submit">
+        </form>
+        </body>
+</html>
