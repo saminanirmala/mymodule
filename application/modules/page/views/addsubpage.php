48f56654e56c38577ca2634aa02d95eb99f87ec7
saminanirmala <samina.tuladhar7@gmail.com>
2014-09-25 21:49:24 +0545

    addsubpage added

--- application/modules/page/views/addsubpage.php
+++ application/modules/page/views/addsubpage.php
@@ -7,15 +7,15 @@
         <form name="" method="POST">
             Sub Page:<input type="text" name="subpage"><br/>
             <label>Parent Page</label>
-                           <select name="pagename">
-                             <option id="0">-SELECT-</option>
-            <?php foreach ($getallpage as $page): ?>
-                               <option value="<?php echo $page['page_id']; ?>"><?php echo $page['page_title']; ?></option>
-                             <?php if($page['parent_id']==1){?>
-                  <option value="<?php echo $page['parent_id'];?>"><?php echo $page['sub_page'];?></option>
-                             <?php }?>
-            <?php endforeach; ?>
-        </select><br/>
+            <select name="pagename">
+                <option id="0">-SELECT-</option>
+                <?php foreach ($getallpage as $page): ?>
+                    <option value="<?php echo $page->page_id; ?>"><?php echo $page->page_title; ?></option>
+
+
+
+                <?php endforeach; ?>
+            </select><br/>
             <input type="submit" name="submit" value="Add">
         </form>
     </body>
